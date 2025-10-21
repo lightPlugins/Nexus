152d73f9544d25d7f1ef500c6eb121cb310a0953
lightplugins <lightningdesign@outloo.de>
2025-10-21 13:13:04 +0200

    removed OreStacks hook

--- nexus-core/src/main/java/io/nexstudios/nexus/bukkit/hooks/orestack/IGeneratorMineEvent.java
+++
@@ -1,7 +0,0 @@
-package io.nexstudios.nexus.bukkit.hooks.orestack;
-
-import io.github.pigaut.orestack.event.GeneratorMineEvent;
-
-public interface IGeneratorMineEvent {
-    void execute(GeneratorMineEvent event);
-}
